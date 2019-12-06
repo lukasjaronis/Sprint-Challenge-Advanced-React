c0a30445b6284b10a4c05211bc5337dcca752e79
lukasjaronis <jaronisluke13@gmail.com>
2019-12-06 11:13:52 -0500

    playercard template done

---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PlayerCard = ({ player }) => {
+    return(
+        <div>
+        <span>Name: </span>
+        <span>Country: </span>
+        <span>Searches: </span>
+        </div>
+    )
+}
+
+export default PlayerCard;
